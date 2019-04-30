72a906d1da1f747b6337ad2129d1c90782737ff9
alexandersavchenko1993 <40425822+alexandersavchenko1993@users.noreply.github.com>
2019-04-30 21:32:25 +0300

    Added factorial with loop
    Factorial without recursive

---
+++ factorial_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+long res;
+n = 5;
+res = 1;
+    for (int i = 1; i<=n; i++){
+         res = res*i;
+     };
+    cout << res <<endl;
+    return 0;
+ }
