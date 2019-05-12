711bbd98b87d139dd129056c9cf98d05fa7f335c
alexandersavchenko1993 <40425822+alexandersavchenko1993@users.noreply.github.com>
2019-05-12 22:21:55 +0300

    Added private_var_hack.cpp
    Private Vars Hacked

---
+++ private_var_hack.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+    A():i(11), s("Alex"){}
+private:
+    int i;
+    string s;
+public:
+    void printA()
+    {
+        cout<<i<<endl;
+        cout<<s<<endl;
+    }
+};
+
+struct Hack
+{
+    int iHack;
+    string sHack;
+};
+
+
+int main(int argc, const char * argv[]) {
+    A a;
+    Hack h;
+    
+    ((Hack*)&a)->iHack = 12;
+    ((Hack*)&a)->sHack = "Bob";
+    
+    reinterpret_cast<Hack*>(&a)->iHack = 777;
+    reinterpret_cast<Hack*>(&a)->sHack = "Max";
+    
+    a.printA();
+    return 0;
+}
