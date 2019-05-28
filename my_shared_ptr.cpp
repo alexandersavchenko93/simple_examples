2251f60927282f35cfab50eeb652df11d91d0752
alexandersavchenko1993 <40425822+alexandersavchenko1993@users.noreply.github.com>
2019-05-28 23:07:18 +0300

    Added shared pointer implementation

---
+++ my_shared_ptr.cpp
@@ -0,0 +1,83 @@
+//  main.cpp
+//  SharedPointer_my
+//
+//  Created by Александр on 5/28/19.
+//  Copyright © 2019 Александр. All rights reserved.
+
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+class SharedPointer
+{
+public:
+    SharedPointer(T* ptr)
+    {
+        //Инициализируем ref - указателем на входящий обьект
+        ref = ptr;
+        //Устанавливаем счетчик ссылок в 1
+        ref_count = new unsigned(1);
+    }
+    
+    SharedPointer(SharedPointer& sptr)
+    {
+        ref = sptr.ref;
+        ref_count = sptr.ref_count;
+        ++(*ref_count);
+        cout<<"Counter: "<<*ref_count<<endl;
+    }
+    
+    ~SharedPointer()
+    {
+        remove();
+    }
+    
+    SharedPointer& operator=(SharedPointer & sptr) {
+        /* Если уже присвоено объекту, удаляем одну ссылку. */
+        if (*ref_count > 0) {
+            remove();
+        }
+        if (this != &sptr) {
+            ref = sptr.ref;
+            ref_count = sptr.ref_count;
+            ++(*ref_count);
+        }
+        return *this;
+    }
+    
+    T operator*() {
+        return *ref;
+    }
+    
+protected:
+    void remove() {
+        --(*ref_count);
+        cout<<"Counter: "<<*ref_count<<endl;
+        if (*ref_count == 0) {
+            delete ref;
+            delete ref_count;
+            ref = nullptr;
+            ref_count = nullptr;
+            cout<<"Object Deleted"<<endl;
+        }
+    }
+     /*Класс интеллектуального указателя нуждается в указателях на собственно
+     себя и на счетчик ссылок. Оба они должны быть указателями, а не реальным
+     объектом или значением счетчика ссылок, так как цель интеллектуального
+     указателя - в подсчете количества ссылок через множество интеллектуальных
+     указателей н один объект.
+     */
+    T* ref;
+    unsigned* ref_count;
+};
+
+int main(int argc, const char * argv[]) {
+    
+    int* a = new int(5);
+    SharedPointer<int> sa(a);
+    SharedPointer<int> sb(sa);
+    SharedPointer<int> sc = sa;
+    sb = sc;
+    return 0;
+}
