6e48f31988f0839ad4156f3be0b49ca470d3e3e3
alexandersavchenko1993 <40425822+alexandersavchenko1993@users.noreply.github.com>
2021-07-09 21:49:20 +0300

    Create 2d_array.cpp

---
+++ 2d_array.cpp
@@ -0,0 +1,23 @@
+//  Created by Александр on 2/25/19.
+//  Copyright © 2019 Александр. All rights reserved.
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, const char* argv[]) {
+
+    // 3 массива по 5 элеменов
+    const unsigned int DIM1 = 3;
+    const unsigned int DIM2 = 5;
+
+    int ary[DIM1][DIM2] = {
+    { 1, 2, 3, 4, 5 },
+    { 2, 4, 6, 8, 10 }, //ary[1][3]
+    { 3, 6, 9, 12, 15 }
+    };
+
+    int elemet = ary[1][3]; // 8
+
+    return 0;
+}
