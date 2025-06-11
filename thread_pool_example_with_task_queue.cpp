f7f67550d2c568c6bf8d8c6f2021675585a2e1a0
Alex <40425822+alexandersavchenko93@users.noreply.github.com>
2025-06-11 22:48:36 +0300

    Update thread_pool_example_with_task_queue.cpp

--- thread_pool_example_with_task_queue.cpp
+++ thread_pool_example_with_task_queue.cpp
@@ -5,47 +5,42 @@
 #include <mutex>
 #include <vector>
 
-// Глобальный для cout так как longCalc находится вне main()
+// !!! Глобальный для cout так как longCalc находится вне main()
 std::mutex cout_mtx;
 
 void longCalc(const std::string& str)
 {
     {
         std::lock_guard<std::mutex> lock(cout_mtx);
-        std::cout << "LongCalc: " << str
-                  << " in thread " << std::this_thread::get_id() << std::endl;
+        std::cout << "LongCalc: " << str << " in thread " << std::this_thread::get_id() << std::endl;
     }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(500)); // имитация долгой работы
+    std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
 }
 
 int main() {
-    std::mutex mtx;
     std::queue<std::string> q_data;
-
     q_data.push("1");
     q_data.push("2");
     q_data.push("3");
     q_data.push("4");
 
+    std::mutex mtx;
     const int num_threads = 4;
     std::vector<std::thread> threads;
-
+    
     for (int i = 0; i < num_threads; ++i) {
         threads.emplace_back([&]() {
             while (true) {
                 std::string str;
-
                 {
                     std::lock_guard<std::mutex> lock(mtx);
                     if (q_data.empty()) {
                         break;
                     }
-
+                    // !!! std::move в этом случае даёт оптимизацию: передаёт строку без копирования.
                     str = std::move(q_data.front());
                     q_data.pop();
                 }
-
                 longCalc(str);
             }
         });
@@ -58,4 +53,3 @@ int main() {
     std::cout << "All done.\n";
     return 0;
 }
-
