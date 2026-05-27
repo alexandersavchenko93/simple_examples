b1c81c613d252426c91acc8f8368f2c15140393e
Alex <40425822+alexandersavchenko93@users.noreply.github.com>
2026-05-27 17:38:41 +0300

    Refactor RationalNumber operators for readability

--- RationalNumber.cpp
+++ RationalNumber.cpp
@@ -11,10 +11,8 @@ public:
         if (den == 0) {
             throw std::invalid_argument("Denominator cannot be zero");
         }
-
         numerator = num;
         denominator = den;
-
         normalize();
     }
 
@@ -27,34 +25,20 @@ public:
     }
 
     RationalNumber operator+(const RationalNumber& other) const {
-        int newNumerator =
-            numerator * other.denominator +
-            other.numerator * denominator;
-
-        int newDenominator =
-            denominator * other.denominator;
-
+        int newNumerator = numerator * other.denominator + other.numerator * denominator;
+        int newDenominator = denominator * other.denominator;
         return RationalNumber(newNumerator, newDenominator);
     }
 
     RationalNumber operator-(const RationalNumber& other) const {
-        int newNumerator =
-            numerator * other.denominator -
-            other.numerator * denominator;
-
-        int newDenominator =
-            denominator * other.denominator;
-
+        int newNumerator = numerator * other.denominator - other.numerator * denominator;
+        int newDenominator = denominator * other.denominator;
         return RationalNumber(newNumerator, newDenominator);
     }
 
     RationalNumber operator*(const RationalNumber& other) const {
-        int newNumerator =
-            numerator * other.numerator;
-
-        int newDenominator =
-            denominator * other.denominator;
-
+        int newNumerator = numerator * other.numerator;
+        int newDenominator = denominator * other.denominator;
         return RationalNumber(newNumerator, newDenominator);
     }
 
@@ -62,17 +46,14 @@ public:
         if (other.numerator == 0) {
             throw std::runtime_error("Division by zero rational number");
         }
-
         int newNumerator = numerator * other.denominator;
         int newDenominator = denominator * other.numerator;
-        
         return RationalNumber(newNumerator, newDenominator);
     }
 
     bool operator==(const RationalNumber& other) const
     {
-        return numerator == other.numerator &&
-               denominator == other.denominator;
+        return numerator == other.numerator && denominator == other.denominator;
     }
 
     bool operator!=(const RationalNumber& other) const
@@ -82,11 +63,9 @@ public:
 
     friend std::ostream& operator<<(std::ostream& os, const RationalNumber& value) {
         os << value.numerator;
-
         if (value.denominator != 1) {
             os << "/" << value.denominator;
         }
-
         return os;
     }
 
@@ -96,8 +75,7 @@ private:
 
     void normalize() {
         // 0/x -> 0/1
-        if (numerator == 0)
-        {
+        if (numerator == 0) {
             denominator = 1;
             return;
         }
@@ -122,20 +100,16 @@ int main() try
     std::cout << "b = " << b << std::endl;
 
     std::cout << "\nAddition:" << std::endl;
-    std::cout << a << " + " << b
-                << " = " << (a + b) << std::endl;
+    std::cout << a << " + " << b << " = " << (a + b) << std::endl;
 
     std::cout << "\nSubtraction:" << std::endl;
-    std::cout << a << " - " << b
-                << " = " << (a - b) << std::endl;
+    std::cout << a << " - " << b << " = " << (a - b) << std::endl;
 
     std::cout << "\nMultiplication:" << std::endl;
-    std::cout << a << " * " << b
-                << " = " << (a * b) << std::endl;
+    std::cout << a << " * " << b << " = " << (a * b) << std::endl;
 
     std::cout << "\nDivision:" << std::endl;
-    std::cout << a << " / " << b
-                  << " = " << (a / b) << std::endl;
+    std::cout << a << " / " << b << " = " << (a / b) << std::endl;
     
     bool res = a == b;
     std::cout << "\n==:" << std::endl;
@@ -143,9 +117,7 @@ int main() try
 
     return 0;
 }
-catch (const std::exception& ex)
-{
-    std::cout << "Exception: "
-              << ex.what() << std::endl;
+catch (const std::exception& ex) {
+    std::cout << "Exception: " << ex.what() << std::endl;
     return -1;
 }
